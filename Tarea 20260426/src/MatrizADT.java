cb5410cfd8068202a4327099ff703f2e9bde9021
2552_Angel_Martinez_2026 <140294076+fotsy501@users.noreply.github.com>
2026-04-29 20:00:27 -0600

    Revert "Add files via upload"

--- Tarea 20260426/src/MatrizADT.java
+++
@@ -1,6 +0,0 @@
-public interface MatrizADT <K> {
-void agregar(int fila, int columna, K valor);
-    K obtener(int fila, int columna); 
-    void mostrar();
-    void agregar1(int fila, int columna, K valor);
-}
